46cff7afe2a5b11d7596f77bf5efb82ef037c639
Michael Bauer <michael.bauer@hs-heilbronn.de>
2017-04-27 15:56:44 +0200

    fixed null pointer ecxeption caused by view in clearSelection()

--- app/src/main/java/de/opti4apps/timelytest/DayListFragment.java
+++ app/src/main/java/de/opti4apps/timelytest/DayListFragment.java
@@ -17,18 +17,27 @@ import android.view.MenuInflater;
 import android.view.MenuItem;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ArrayAdapter;
+import android.widget.Spinner;
 
 import org.greenrobot.eventbus.EventBus;
 import org.greenrobot.eventbus.Subscribe;
+import org.joda.time.DateTime;
+import org.joda.time.LocalDate;
+import org.joda.time.format.DateTimeFormat;
+import org.joda.time.format.DateTimeFormatter;
 
 import java.util.ArrayList;
+import java.util.Date;
 import java.util.List;
 import java.util.Map;
 
 import butterknife.BindView;
 import butterknife.ButterKnife;
 import butterknife.OnClick;
+import butterknife.OnItemSelected;
 import de.opti4apps.timelytest.data.Day;
+import de.opti4apps.timelytest.data.Day_;
 import de.opti4apps.timelytest.event.DayDatasetChangedEvent;
 import de.opti4apps.timelytest.event.DayMultibleSelectionEvent;
 import de.opti4apps.timelytest.event.DaySelectedEvent;
@@ -43,14 +52,23 @@ public class DayListFragment extends Fragment {
 
     public static final String TAG = DayListFragment.class.getSimpleName();
     private final List<Day> mDayList = new ArrayList<>();
+    private final String[] mMonthArray = new String[12];
+    private final String[] mYearArray = new String[35];
     @BindView(R.id.list)
     RecyclerView mRecyclerView;
     @BindView(R.id.fab)
     FloatingActionButton mFloatingActionButton;
+    @BindView(R.id.year_spinner)
+    Spinner mYearSpinner;
+    @BindView(R.id.month_spinner)
+    Spinner mMonthSpinner;
     private Box<Day> mDayBox;
     private Query<Day> mDayQuery;
     private ActionMode mActionMode;
     private ActionModeCallback mActionModeCallback = new ActionModeCallback();
+    private int mCurrentMonthArrayPosition = -1;
+    private int mCurrentYearArrayPosition = -1;
+
 
     /**
      * Mandatory empty constructor for the fragment manager to instantiate the
@@ -68,13 +86,28 @@ public class DayListFragment extends Fragment {
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         mDayBox = ((App) getActivity().getApplication()).getBoxStore().boxFor(Day.class);
-        mDayQuery = mDayBox.query().build();
+        mDayQuery = mDayBox.query().orderDesc(Day_.day).build();
         EventBus.getDefault().register(this);
 
         mDayList.addAll(mDayQuery.find());
         setHasOptionsMenu(true);
         setRetainInstance(true);
+        initArrays();
 
+    }
+
+    private void initArrays() {
+        LocalDate date;
+
+        for (int i = 0; i < mMonthArray.length; i++) {
+            date = new LocalDate(0, i + 1, 1);
+            mMonthArray[i] = date.monthOfYear().getAsShortText();
+        }
+
+        for (int i = 0; i < mYearArray.length; i++) {
+            date = new LocalDate(2000 + i, 1, 1);
+            mYearArray[i] = date.year().getAsShortText();
+        }
     }
 
     @Override
@@ -83,14 +116,25 @@ public class DayListFragment extends Fragment {
         View view = inflater.inflate(R.layout.fragment_day_list, container, false);
         ButterKnife.bind(this, view);
 
-        // if (view instanceof RecyclerView) {
         Context context = view.getContext();
         mRecyclerView.setLayoutManager(new LinearLayoutManager(context));
         mRecyclerView.setAdapter(new MyDayRecyclerViewAdapter(mDayList));
         mRecyclerView.setHasFixedSize(true);
-        //setHasOptionsMenu(true);
 
-        //}
+        ArrayAdapter<String> monthAdapter = new ArrayAdapter<>(getContext(), android.R.layout.simple_spinner_item, mMonthArray);
+        monthAdapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+        mMonthSpinner.setAdapter(monthAdapter);
+        if (mCurrentMonthArrayPosition == -1)
+            mCurrentMonthArrayPosition = monthAdapter.getPosition(LocalDate.now().monthOfYear().getAsShortText());
+        mMonthSpinner.setSelection(mCurrentMonthArrayPosition);
+
+        ArrayAdapter<String> yearAdapter = new ArrayAdapter<>(getContext(), android.R.layout.simple_spinner_item, mYearArray);
+        yearAdapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+        mYearSpinner.setAdapter(yearAdapter);
+        if (mCurrentYearArrayPosition == -1)
+            mCurrentYearArrayPosition = yearAdapter.getPosition(LocalDate.now().year().getAsShortText());
+        mYearSpinner.setSelection(mCurrentYearArrayPosition);
+
         return view;
     }
 
@@ -113,6 +157,29 @@ public class DayListFragment extends Fragment {
     @OnClick(R.id.fab)
     public void onFABClicked() {
         EventBus.getDefault().post(new DaySelectedEvent(0));
+    }
+
+    @OnItemSelected(R.id.month_spinner)
+    public void onMonthSpinnerItemSelected(Spinner spinner, int position) {
+        mCurrentMonthArrayPosition = position;
+        spinnerItemSelected();
+    }
+
+    @OnItemSelected(R.id.year_spinner)
+    public void onYearSpinnerItemSelected(Spinner spinner, int position) {
+        mCurrentYearArrayPosition = position;
+        spinnerItemSelected();
+    }
+
+    private void spinnerItemSelected() {
+        String dateString = mMonthSpinner.getAdapter().getItem(mCurrentMonthArrayPosition).toString() + mYearSpinner.getAdapter().getItem(mCurrentYearArrayPosition).toString();
+        Log.d(TAG, dateString);
+        DateTimeFormatter formatter = DateTimeFormat.forPattern("MMMyyyy");
+        DateTime dateTime = DateTime.parse(dateString, formatter);
+        Date min = dateTime.withDayOfMonth(1).toDate();
+        Date max = dateTime.withDayOfMonth(1).plusMonths(1).minusDays(1).toDate();
+        mDayQuery = mDayBox.query().between(Day_.day, min, max).orderDesc(Day_.day).build();
+        EventBus.getDefault().post(new DayDatasetChangedEvent(TAG));
     }
 
     @Subscribe
